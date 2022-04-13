3b1ee94def228e6f986bc537e7ee79479ae42007
staguero <89417516+staguero@users.noreply.github.com>
2022-04-13 01:01:13 -0300

    Upload código funcional A*-12abril

---
+++ A_Star.py
@@ -0,0 +1,188 @@
+class A_Star():
+    def __init__(self,mapa,current_node,target_node):
+        self.close_list=[]
+        self.open_list=[]
+        self.mapa = mapa
+        self.current_node = current_node
+        self.target_node = target_node
+    
+    def in_close_list(self,node):
+        if node in self.close_list:
+            return node
+        else:
+            return False
+    
+    def in_open_list(self,node):
+        if node in self.open_list:
+            return node
+        else:
+            return False
+
+    def check_list(self,i,j):
+        if self.mapa[i][j].id is not None:
+            if self.mapa[i][j].id[0:5] == "Shelf":
+                if self.mapa[i][j].id == self.target_node.id: #debo agregar el objetivo a lista abierta
+                    if (self.mapa[i][j].parent == self.current_node): #si el padre es el nodo inicial no lo guardo
+                        return
+                else:
+                    return
+        if self.in_close_list(self.mapa[i][j]):
+            return
+        
+        self.mapa[i][j].G = self.mapa[i][j].parent.G + ( (self.mapa[i][j].parent.x-self.mapa[i][j].x)**2 + (self.mapa[i][j].parent.y-self.mapa[i][j].y)**2 ) **0.5
+        self.mapa[i][j].H = ( (self.target_node.x-self.mapa[i][j].x)**2 + (self.target_node.y-self.mapa[i][j].y)**2 ) **0.5
+
+        prev = self.in_open_list(self.mapa[i][j])
+        if prev:
+            if (self.mapa[i][j].G + self.mapa[i][j].H) <= (prev.G + prev.H):
+                self.open_list.remove(prev)
+                self.open_list.append(self.mapa[i][j])
+        else:
+            self.open_list.append(self.mapa[i][j])
+
+    def update_parent(self,old_parent,new_candidate):
+        if old_parent.parent is not None:
+            if old_parent.id==self.target_node.id:          #Modificación nodo padre del objetivo se actualice si el actual es el nodo inicial
+                if old_parent.parent==self.current_node:    #aunque la distancia del nuevo padre sea mayor (obvio si están juntos los casilleros)
+                    return True 
+            if (old_parent.parent.G>new_candidate.G):
+                return True
+            else:
+                return False
+        else:
+            return True
+
+    def min_F(self):
+        try:
+            minF = self.open_list[0]
+            for node in self.open_list:
+                if (node.G + node.H) <= (minF.G + minF.H):
+                    if node.id==self.target_node.id:
+                        if (self.target_node.parent!=self.current_node):    #modificación por lo mismo
+                            minF = node                                     #si el mínimo es el actual pero su nodo es el padre, no lo uses
+                    else:
+                        minF = node
+            return minF
+        except:
+            print(self.open_list)
+    
+    def start_method(self,mapa_columnas,mapa_filas):
+        path_list = []
+        self.open_list.append(self.current_node)
+        while True:
+            tmp = self.in_close_list(self.target_node)
+            if tmp:
+                path_list = [[tmp.x, tmp.y]]
+                while tmp.id is not self.current_node.id:
+                    tmp = tmp.parent
+                    path_list.append([tmp.x, tmp.y])
+
+                path_list.reverse()
+                return path_list
+            else:
+                minF = self.min_F()
+                self.close_list.append(minF)
+                self.open_list.remove(minF)
+                for j in range(mapa_columnas):
+                    for i in range(mapa_filas):
+                        if self.mapa[i][j].x == minF.x:
+                            if self.mapa[i][j].y == minF.y:
+                                if (j>=mapa_columnas-1):
+                                    if (i>0):
+                                        if (i<mapa_filas-1):
+                                            if (self.update_parent(self.mapa[i-1][j-1],minF)):
+                                                self.mapa[i-1][j-1].parent = minF
+                                            if (self.update_parent(self.mapa[i][j-1],minF)):
+                                                self.mapa[i][j-1].parent   = minF
+                                            if (self.update_parent(self.mapa[i-1][j],minF)):
+                                                self.mapa[i-1][j].parent   = minF
+                                            if (self.update_parent(self.mapa[i+1][j],minF)):
+                                                self.mapa[i+1][j].parent   = minF
+                                            if (self.update_parent(self.mapa[i+1][j-1],minF)):
+                                                self.mapa[i+1][j-1].parent = minF 
+                                        else:
+                                            if (self.update_parent(self.mapa[i-1][j-1],minF)):
+                                                self.mapa[i-1][j-1].parent = minF
+                                            if (self.update_parent(self.mapa[i][j-1],minF)):
+                                                self.mapa[i][j-1].parent   = minF
+                                            if (self.update_parent(self.mapa[i-1][j],minF)):
+                                                self.mapa[i-1][j].parent   = minF
+                                    else:
+                                        if (self.update_parent(self.mapa[i][j-1],minF)):
+                                            self.mapa[i][j-1].parent   = minF
+                                        if (self.update_parent(self.mapa[i+1][j],minF)):
+                                            self.mapa[i+1][j].parent   = minF
+                                        if (self.update_parent(self.mapa[i+1][j-1],minF)):
+                                            self.mapa[i+1][j-1].parent = minF                                      
+                                if (j<=0) :
+                                    if (i<=0):
+                                        if (self.update_parent(self.mapa[i][j+1],minF)):
+                                            self.mapa[i][j+1].parent   = minF
+                                        if (self.update_parent(self.mapa[i+1][j],minF)):
+                                            self.mapa[i+1][j].parent   = minF
+                                        if (self.update_parent(self.mapa[i+1][j+1],minF)):
+                                            self.mapa[i+1][j+1].parent = minF
+                                    if (i>0):
+                                        if (i<mapa_filas-1):
+                                            if (self.update_parent(self.mapa[i-1][j],minF)):
+                                                self.mapa[i-1][j].parent   = minF
+                                            if (self.update_parent(self.mapa[i-1][j+1],minF)):
+                                                self.mapa[i-1][j+1].parent = minF
+                                            if (self.update_parent(self.mapa[i][j+1],minF)):
+                                                self.mapa[i][j+1].parent   = minF
+                                            if (self.update_parent(self.mapa[i+1][j],minF)):
+                                                self.mapa[i+1][j].parent   = minF
+                                            if (self.update_parent(self.mapa[i+1][j+1],minF)):
+                                                self.mapa[i+1][j+1].parent = minF
+                                        else:
+                                            if (self.update_parent(self.mapa[i-1][j],minF)):
+                                                self.mapa[i-1][j].parent   = minF
+                                            if (self.update_parent(self.mapa[i-1][j+1],minF)):
+                                                self.mapa[i-1][j+1].parent = minF
+                                            if (self.update_parent(self.mapa[i][j+1],minF)):
+                                                self.mapa[i][j+1].parent   = minF
+                                if  (j<mapa_columnas-1) and (j>0):
+                                    if (i>0):
+                                        if (i<mapa_filas-1):
+                                            if (self.update_parent(self.mapa[i+1][j],minF)):
+                                                self.mapa[i+1][j].parent   = minF
+                                            if (self.update_parent(self.mapa[i+1][j-1],minF)):
+                                                self.mapa[i+1][j-1].parent = minF
+                                            if (self.update_parent(self.mapa[i+1][j+1],minF)):
+                                                self.mapa[i+1][j+1].parent = minF
+                                            if (self.update_parent(self.mapa[i-1][j],minF)):
+                                                self.mapa[i-1][j].parent   = minF
+                                            if (self.update_parent(self.mapa[i-1][j-1],minF)):
+                                                self.mapa[i-1][j-1].parent = minF
+                                            if (self.update_parent(self.mapa[i-1][j+1],minF)):
+                                                self.mapa[i-1][j+1].parent = minF
+                                            if (self.update_parent(self.mapa[i][j-1],minF)):
+                                                self.mapa[i][j-1].parent   = minF
+                                            if (self.update_parent(self.mapa[i][j+1],minF)):
+                                                self.mapa[i][j+1].parent   = minF
+                                        else:
+                                            if (self.update_parent(self.mapa[i-1][j],minF)):
+                                                self.mapa[i-1][j].parent   = minF
+                                            if (self.update_parent(self.mapa[i-1][j-1],minF)):
+                                                self.mapa[i-1][j-1].parent = minF
+                                            if (self.update_parent(self.mapa[i-1][j+1],minF)):
+                                                self.mapa[i-1][j+1].parent = minF
+                                            if (self.update_parent(self.mapa[i][j-1],minF)):
+                                                self.mapa[i][j-1].parent   = minF
+                                            if (self.update_parent(self.mapa[i][j+1],minF)):
+                                                self.mapa[i][j+1].parent   = minF
+                                    else:
+                                            if (self.update_parent(self.mapa[i+1][j],minF)):
+                                                self.mapa[i+1][j].parent   = minF
+                                            if (self.update_parent(self.mapa[i+1][j-1],minF)):
+                                                self.mapa[i+1][j-1].parent = minF
+                                            if (self.update_parent(self.mapa[i+1][j+1],minF)):
+                                                self.mapa[i+1][j+1].parent = minF
+                                            if (self.update_parent(self.mapa[i][j-1],minF)):
+                                                self.mapa[i][j-1].parent   = minF
+                                            if (self.update_parent(self.mapa[i][j+1],minF)):
+                                                self.mapa[i][j+1].parent   = minF
+                for j in range(mapa_columnas):
+                    for i in range(mapa_filas):
+                        if  (self.mapa[i][j].parent==minF):
+                            self.check_list(i,j)
---
+++ Dibujar.py
@@ -0,0 +1,57 @@
+import tkinter as tk
+import numpy as np
+
+class Dibujar():
+    def __init__(self,lado,columna,fila,columnas_estante,estantes_f,estantes_c):
+        self.lado=lado
+        self.fila=fila
+        self.columna=columna
+        self.columnas_estante=columnas_estante
+        self.estantes_f=estantes_f
+        self.estantes_c=estantes_c
+        self.casilleros=[]
+        self.camino=[]
+        
+        self.ventana=tk.Tk()
+        self.ventana.title("ÁREA DE TRABAJO")
+        self.ventana.iconbitmap("ico.ico")
+        #Ventana con longitud requerida
+        self.ventana.geometry(f"{str(lado*self.columna)}x{str(lado*self.fila)}")
+        self.ventana.resizable(0,0) #evita que se agrande o minimice
+
+        self.interfaz= tk.Canvas(self.ventana)
+        self.interfaz.pack(fill="both",expand=True)
+
+    def asignar_posiciones_casilleros(self):
+        fin_columnas=self.columna-(2+self.columnas_estante)
+        m=np.linspace(2,fin_columnas,self.estantes_c)
+        fin_filas=self.fila-(2+2)
+        m2=np.linspace(2,fin_filas,self.estantes_f)
+        for j in m:
+            for i in m2:
+                col=int(j)
+                for k in range(self.columnas_estante): 
+                    self.casilleros.append([int(i),col])
+                    self.casilleros.append([int(i+1),col])
+                    col+=1
+
+    def definir_camino(self,camino):
+        self.camino=camino
+    
+    def dibujar_mapa(self):
+        for i in range(self.columna):
+            for j in range(self.fila):
+                    if [j,i] in self.casilleros:
+                        self.interfaz.create_rectangle(i*self.lado,j*self.lado,(i+1)*self.lado,(j+1)*self.lado,fill="#2F4F4F")
+                    else:
+                        self.interfaz.create_rectangle(i*self.lado,j*self.lado,(i+1)*self.lado,(j+1)*self.lado,fill="#698B69")
+
+    def dibujar_mapa_full(self):
+        for i in range(self.columna):
+            for j in range(self.fila):
+                    if [j,i] in self.camino:
+                        self.interfaz.create_rectangle(i*self.lado,j*self.lado,(i+1)*self.lado,(j+1)*self.lado,fill="#76EE00")
+                    #elif [j,i] in self.casilleros:
+                    #    self.interfaz.create_rectangle(i*self.lado,j*self.lado,(i+1)*self.lado,(j+1)*self.lado,fill="#2F4F4F")
+                    #else:
+                    #    self.interfaz.create_rectangle(i*self.lado,j*self.lado,(i+1)*self.lado,(j+1)*self.lado,fill="#698B69")
